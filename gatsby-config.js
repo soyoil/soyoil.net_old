b6c359d5412a5ac5b78a461b56c0e618dff1e98e
soyoil <kappamaki8801@gmail.com>
2020-04-19 16:53:21 +0900

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `soyoil.net`,
+    description: `homepage`,
+    author: `soyoil`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `soyoil.net`,
+        short_name: `soyoil.net`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
