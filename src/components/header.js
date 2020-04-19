8cabeaa2e764acc5b87fde141b1e5cc15e6ab3d9
soyoil <kappamaki8801@gmail.com>
2020-04-19 18:39:10 +0900

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,32 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => (
+  <header className="navigation">
+    <div class="container navlinks">
+      <p className="navitems">
+        <Link to="/">home</Link>
+      </p>
+      <p className="navitems">
+        <Link to="/">works</Link>
+      </p>
+      <p className="navitems">
+        <Link to="/blog">blog</Link>
+      </p>
+      <p className="navitems">
+        <Link to="/about">about</Link>
+      </p>
     </div>
   </header>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,47 +5,28 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import "normalize.css";
+import "milligram";
 
-import Header from "./header"
-import "./layout.css"
+import "./layout.css";
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <div style={{ fontFamily: "sans-serif" }}>
+    <Helmet>
+      {/* <link
+        rel="stylesheet"
+        href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
+      /> */}
+    </Helmet>
+    <div class="container">{children}</div>
+  </div>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
